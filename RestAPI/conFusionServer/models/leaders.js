4fcd4f15969f2fa507996775ca2b3896141d8a4e
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2022-12-04 09:01:59 -0800

    Assignment 2 MongoDB
    Assignment 2 MongoDB

---
+++ RestAPI/conFusionServer/models/leaders.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const LeaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr:{
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+}, {
+    timestamps: true
+});
+
+
+
+var Leaders = mongoose.model('Leaders', LeaderSchema);
+
+module.exports = Leaders;

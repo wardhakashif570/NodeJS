717a16bb954527e53f300695a9e698b77ef3dd5f
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2022-12-12 08:16:04 -0800

    Passport
    Passport

--- RestAPI/conFusionServer/app.js
+++ RestAPI/conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 const cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +12,9 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const mongoose = require('mongoose');
 
@@ -46,25 +51,17 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
-
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
@@ -72,8 +69,7 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

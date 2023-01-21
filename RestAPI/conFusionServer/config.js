5bffdedad0cb28351bb6906138c14ea992854b6b
wardha kashif <50556244+wardhakashif570@users.noreply.github.com>
2023-01-21 05:10:15 -0800

    Passport Facebook
    Passport Facebook

--- RestAPI/conFusionServer/config.js
+++ RestAPI/conFusionServer/config.js
@@ -1,4 +1,9 @@
+
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '709414000576449',
+        clientSecret: 'b02d60c24e3d2ed03f0e2a7a1eb57f3b'
+    }
 }
--- RestAPI/conFusionServer/models/user.js
+++ RestAPI/conFusionServer/models/user.js
@@ -19,6 +19,7 @@ var User = new Schema({
     type: String,
     default: ''
   },
+  facebookId: String,
   admin: {
     type: Boolean,
     default: false
